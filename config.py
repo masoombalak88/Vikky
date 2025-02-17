4ccc0790416ba71dd0b2aa0ed443d533bad00990
𓄂️𝆺𝅥⃝☠️ ⃪ͥ͢ ᷟ 𝐌͢ꝛ♥️꯭⃝⃨⃜⃝𝐕𝐈𝐊𝐊𝐘༏⃪͢🔥᪵᪳❛𝆺꯭𝅥༎ࠫ <vikashyadav735556@gmail.com>
2025-02-17 13:47:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Kingdk88/SukkuMusic",
+    "https://github.com/masoombalak88/Vikky",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
